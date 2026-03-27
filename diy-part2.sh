aa46ed5c01fed0490bb16f4c6a8e03269d27e44e
marchmin <462887806@qq.com>
2026-03-27 23:56:53 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,4 +12,4 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
-./scripts/feeds -p printing -d m
+./scripts/feeds install -a -p printing -d m
