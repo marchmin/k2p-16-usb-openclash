c97ee135dae4f6a2cec6d40612997db013117638
marchmin <462887806@qq.com>
2026-03-30 06:53:08 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -22,4 +22,5 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo "src-git printing https://github.com/marchmin/lede-cups.git" >> feeds.conf.default
 #echo "src-git printing https://github.com/marchmin/openwrt-printing-packages.git" >> feeds.conf.default
-echo "src-git cups https://github.com/TheMMcOfficial/lede-cups.git" >> feeds.conf.default
+#echo "src-git cups https://github.com/TheMMcOfficial/lede-cups.git" >> feeds.conf.default
+echo "src-git cups https://github.com/lllrrr/lede-cups.git" >> feeds.conf.default
