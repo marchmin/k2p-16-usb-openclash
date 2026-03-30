9a5f349ef798c06bcbaa51ccf2049f084d9e676c
marchmin <462887806@qq.com>
2026-03-31 00:03:55 +0800

    Add files via upload

---
+++ diy-part1-1.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+#git clone https://github.com/vernesong/OpenClash.git package/OpenClash
+#git clone https://github.com/marchmin/openwrt-printing-packages.git package/printing
+#git clone https://github.com/marchmin/lede-cups.git package/printing
+
+# Add a feed source
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#echo "src-git printing https://github.com/marchmin/lede-cups.git" >> feeds.conf.default
+#echo "src-git printing https://github.com/marchmin/openwrt-printing-packages.git" >> feeds.conf.default
+#echo "src-git cups https://github.com/TheMMcOfficial/lede-cups.git" >> feeds.conf.default
+#echo "src-git printing  https://github.com/lllrrr/lede-cups.git" >> feeds.conf.default
