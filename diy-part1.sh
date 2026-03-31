e67e572de54d210e4ff81630f6a5dc7383fa611e
marchmin <462887806@qq.com>
2026-03-31 12:39:11 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -23,4 +23,5 @@
 #echo "src-git printing https://github.com/marchmin/lede-cups.git" >> feeds.conf.default
 #echo "src-git printing https://github.com/marchmin/openwrt-printing-packages.git" >> feeds.conf.default
 #echo "src-git cups https://github.com/TheMMcOfficial/lede-cups.git" >> feeds.conf.default
-echo "src-git cups  https://github.com/lllrrr/lede-cups.git" >> feeds.conf.default
+#echo "src-git cups  https://github.com/lllrrr/lede-cups.git" >> feeds.conf.default
+echo "src-git printing-packages https://gitee.com/master0123/openwrt-printing-packages.git;master" >> feeds.conf.default
